46cec2af49b450418f16659985a06b4c51eaf913
Fefe <mynameisfefe@protonmail.com>
2021-04-03 22:38:59 -0300

    added highscore route

--- src/components/Highscore.js
+++ src/components/Highscore.js
@@ -1,6 +1,54 @@
+import {useState, useEffect} from 'react'
+import './highscore.css'
+import firestore from "./firebaseconfig.js";
+const db = firestore;
+const docRef = db.collection("HighScore").doc("Level 1");
+
+
+
 function Highscore () {
+  const [scores,setScores] =useState([])
+
+    docRef
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        const obj = doc.data()
+        let newScores = []
+          for (let property in obj) {        
+          let a = {
+            ranker:property,
+            score:obj[property]
+          }       
+          newScores.push(a)          
+        }      
+        newScores.sort((a,b)=>{
+          return (a.score > b.score)
+        })
+        setScores(newScores)
+      } 
+    })
+    .catch((error) => {
+      console.log("Error getting document:", error);
+    });
+ 
+ 
+
   return (
-    <div></div>
+    <div>
+      <h1>Highscore</h1>
+      <h2>Level 1</h2>
+      <ul>
+        {scores.map((score)=>{
+          return (
+            <li>
+              <h4>{score.ranker}</h4>
+              <h4>{score.score} secs</h4>
+            </li>
+          )
+        })}
+      </ul>
+    </div>
   )
 }
 
--- src/components/WinAlert.js
+++ src/components/WinAlert.js
@@ -1,15 +1,34 @@
-function WinAlert (props) {
+import firestore from "./firebaseconfig.js";
+const db = firestore;
+const docRef = db.collection("HighScore").doc("Level 1");
 
+function WinAlert (props) {
+  function addScore () {
+    const name = document.getElementById("name").value 
+    const scoreLabel = document.getElementById("score").textContent
+    const score = scoreLabel.replace("You Passed! in ", "")
+
+    return docRef.update({
+      [ name]: score
+    })
+    .then(() => {
+      console.log("Document successfully updated!");
+    })
+    .catch((error) => {
+      // The document probably doesn't exist.
+      console.error("Error updating document: ", error);
+    });
+  }
   return(
       <div id="modalWin" className="modalWin">
         <div className="instructions-content winContent">    
             
-          <label>You Passed! in {props.score}</label>       
+          <label id="score">You Passed! in {props.score}</label>       
           <div className="score">
             <label>Enter your name</label>
-            <input ></input>
+            <input id="name"></input>
           </div> 
-          <button className="buttons">Add Score to Highscore</button>
+          <button onClick={addScore} className="buttons">Add Score to Highscore</button>
         </div>
       </div>
   )
